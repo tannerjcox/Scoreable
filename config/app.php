de82f60a3fdd4e2fdf74fd06188b99806c3c7b79
Tanner Cox <tannercox@outlook.com>
2017-03-03 20:20:29 -0700

    Add larval collective and boot form

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+        AdamWathan\BootForms\BootFormsServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +227,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'BootForm' => AdamWathan\BootForms\Facades\BootForm::class
 
     ],
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,13 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <aside class="col-md-3">
+                You have {{ Auth::user()->games()->count() ?: 'no' }} game(s)<br>
+                {!! Form::button('Create Game', ['class' => 'btn btn-primary', 'data-create-game']) !!}
+            </aside>
+            <div class="col-md-9">
                 <div class="panel panel-default">
                     <div class="panel-heading">Dashboard</div>
-
                     <div class="panel-body">
                         You are logged in!
                     </div>
